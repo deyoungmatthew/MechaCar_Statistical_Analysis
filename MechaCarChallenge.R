e2e8b7941684c967a497ce11b171ef3299bc9352
Matthew DeYoung <deyoungmatthewd@gmail.com>
2021-06-06 15:23:47 -0400

    finished deliverable 2 coding

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,25 @@
 library(dplyr)
 
+
+#Deliverable 1
+
+#Read CSV into table
 mechatable <- read.csv(file="MechaCar_mpg.csv", check.names = F, stringsAsFactors = F)
 
-lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=mechatable)
+#Multi Linear Regression for all six columns
+lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + vehicle_length,data=mechatable)
 
-summary(lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=mechatable))
+#Summary of the linear regression
+summary(lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + vehicle_length,data=mechatable))
+
+
+#Deliverable 2
+#Read in Suspension_Coil.csv file
+suspension_table <- read.csv(file="Suspension_Coil.csv", check.names = F, stringsAsFactors = F)
+
+#Group suspension coil's PSI continuous variable across all manufacturing lots
+total_summary <- suspension_table %>% data.frame()%>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+#PSI metrics for each lot: 
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), StdDev = sd(PSI), .groups = "keep")
+
