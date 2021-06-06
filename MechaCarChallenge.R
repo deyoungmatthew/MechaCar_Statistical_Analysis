8acba36b1c85b1ab26e5516e7905cdb0471aee09
Matthew DeYoung <deyoungmatthewd@gmail.com>
2021-06-06 15:34:43 -0400

    finished deliverable 3 coding

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,6 +13,8 @@ lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + vehicle_lengt
 summary(lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + vehicle_length,data=mechatable))
 
 
+
+
 #Deliverable 2
 #Read in Suspension_Coil.csv file
 suspension_table <- read.csv(file="Suspension_Coil.csv", check.names = F, stringsAsFactors = F)
@@ -23,3 +25,15 @@ total_summary <- suspension_table %>% data.frame()%>% summarize(Mean=mean(PSI),M
 #PSI metrics for each lot: 
 lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), StdDev = sd(PSI), .groups = "keep")
 
+
+
+
+
+#Deliverable 3
+# T-Test if manufacturing lots is statistically different from mean population of 1500 PSI
+t.test(suspension_table$PSI, mu = 1500)
+
+#PSI for each manufacturing lot is statistically different from population mean of 1500 PSI
+t.test(subset(suspension_table, Manufacturing_Lot == "Lot1")$PSI, mu = 1500)
+t.test(subset(suspension_table, Manufacturing_Lot == "Lot2")$PSI, mu = 1500)
+t.test(subset(suspension_table, Manufacturing_Lot == "Lot3")$PSI, mu = 1500)
